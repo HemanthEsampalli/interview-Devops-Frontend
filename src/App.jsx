a2553d7ef350909faf9da5ff71f12abe7c6ab749
surajcode7 <skoli151100@gmail.com>
2026-01-01 14:50:35 +0530

    added projects sections for assign project to employee and displaying old projects of that employee

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import AddEmployeeBankDetails from "./pages/admin/AddEmployeeBankDetails";
 import AddEmployeeSalaryDetails from "./pages/admin/AddEmployeeSalaryDetails";
 import EmployeeSalaryDetails from "./pages/admin/EmployeeSalaryDetails";
 import EmployeeBankDetails from "./pages/admin/EmployeeBankDetails";
+import EmployeeProjects from "./pages/admin/EmployeeProjects";
 
 // Admin forms
 import AddAdminForm from "./components/users/AddAdminForm";
@@ -107,6 +108,10 @@ export default function App() {
         <Route
           path="user-lists/employees/:employeeId/add-salary-details"
           element={<AddEmployeeSalaryDetails />}
+        />
+        <Route
+          path="user-lists/employees/:employeeId/projects"
+          element={<EmployeeProjects />}
         />
         //......................
         {/* EMPLOYEE TABS */}
